1ab93e42fc33cf1254dcb471b863c143d7f9d56d
vps <vpsinbox@mail.ru>
2017-11-22 16:07:50 +0300

    1. Перенёс в панель инструментов кнопку получения новых постов. 2. Добавил перевод на русский язык.

--- app/src/main/java/ru/vps/retrofit2test/fragment/MainFragment.java
+++ app/src/main/java/ru/vps/retrofit2test/fragment/MainFragment.java
@@ -8,9 +8,11 @@ import android.support.v4.app.Fragment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ProgressBar;
 
 import java.io.Serializable;
@@ -28,18 +30,21 @@ import ru.vps.retrofit2test.model.Post;
 public class MainFragment extends Fragment {
     private static final String EXTRA_POSTS = "ru.vps.retrofit2test.extra.posts";
     private static final int MESSAGE_SHOW_TIME = 10000;
+    private static final int NUMBER_OF_REQUESTED_POSTS = 30;
     //
     private List<Post> posts = new ArrayList<>();
     //
     private RecyclerView postsView;
-    private Button getPosts;
     private ProgressBar loadingProgress;
-    //
-    private Call<List<Post>> getPostsCall;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
 
         postsView = (RecyclerView) rootView.findViewById(R.id.posts);
@@ -49,69 +54,24 @@ public class MainFragment extends Fragment {
 
         loadingProgress = (ProgressBar) rootView.findViewById(R.id.loadingProgress);
 
-        getPosts = (Button) rootView.findViewById(R.id.getPosts);
-        getPosts.setOnClickListener(new View.OnClickListener() {
-            private static final int NUMBER_OF__POSTS = 15;
-
-            @Override
-            public void onClick(final View v) {
-                showGetPostsButton(false);
-
-                getPostsCall = App.getUmoriliApi().getRandomPosts(NUMBER_OF__POSTS);
-                getPostsCall.enqueue(new Callback<List<Post>>() {
-                    @Override
-                    public void onResponse(@NonNull Call<List<Post>> call, @NonNull Response<List<Post>> response) {
-                        showGetPostsButton(true);
-
-                        if (!response.isSuccessful()) {
-                            return;
-                        }
-
-                        List<Post> body = response.body();
-
-                        if (body != null) {
-                            //TODO возможно настройку ссылок надо перенести на уровень формирования объектов Post из JSON
-                            configuringLinks(body, response);
-
-                            posts.clear();
-                            posts.addAll(body);
-
-                            postsView.setAdapter(new PostsRecyclerAdapter(posts));
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(@NonNull Call<List<Post>> call, @NonNull Throwable t) {
-                        showGetPostsButton(true);
-
-                        final Snackbar snackbar = Snackbar.make(v, t.getLocalizedMessage(), MESSAGE_SHOW_TIME);
-                        snackbar.setAction(R.string.ok, v1 -> snackbar.dismiss());
-                        snackbar.show();
-                    }
-
-                    private void configuringLinks(List<Post> posts, Response<List<Post>> response) {
-                        String server = getServerUrl(response);
-                        for (Post post : posts) {
-                            String postLink = post.getLink();
-                            if (postLink != null) {
-                                String postUrl = server + postLink;
-                                post.setLink(postUrl);
-                            }
-                        }
-                    }
-
-                    private String getServerUrl(Response<List<Post>> response) {
-                        okhttp3.Response rawResponse = response.raw();
-                        String url = rawResponse.request().url().toString();
-                        String urlPath = rawResponse.request().url().encodedPath();
-
-                        return url.substring(0, url.indexOf(urlPath));
-                    }
-                });
-            }
-        });
-
         return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.menu_fragment_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_refresh:
+                doGetPosts();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -130,28 +90,72 @@ public class MainFragment extends Fragment {
             List<Post> temp = (List<Post>) savedInstanceState.getSerializable(EXTRA_POSTS);
             posts = temp;
             postsView.setAdapter(new PostsRecyclerAdapter(posts));
-        }
-    }
-
-    @Override
-    public void onPause() {
-        super.onPause();
-
-        if (getPostsCall.isExecuted()) {
-            getPostsCall.cancel();
-
-            showGetPostsButton(true);
-        }
-    }
-
-    //TODO надо как-то правильно обозвать метод
-    private void showGetPostsButton(boolean flag) {
-        if (flag) {
-            getPosts.setVisibility(View.VISIBLE);
-            loadingProgress.setVisibility(View.GONE);
         } else {
-            getPosts.setVisibility(View.GONE);
-            loadingProgress.setVisibility(View.VISIBLE);
+            doGetPosts();
         }
+    }
+
+    private void showProgressBar(boolean flag) {
+        if (flag) {
+            loadingProgress.setVisibility(View.VISIBLE);
+        } else {
+            loadingProgress.setVisibility(View.GONE);
+        }
+    }
+
+    private void doGetPosts() {
+        showProgressBar(true);
+
+        Call<List<Post>> getPostsCall = App.getUmoriliApi().getRandomPosts(NUMBER_OF_REQUESTED_POSTS);
+        getPostsCall.enqueue(new Callback<List<Post>>() {
+            @Override
+            public void onResponse(@NonNull Call<List<Post>> call, @NonNull Response<List<Post>> response) {
+                showProgressBar(false);
+
+                if (!response.isSuccessful()) {
+                    return;
+                }
+
+                List<Post> body = response.body();
+
+                if (body != null) {
+                    //TODO возможно настройку ссылок надо перенести на уровень формирования объектов Post из JSON
+                    configuringLinks(body, response);
+
+                    posts.clear();
+                    posts.addAll(body);
+
+                    postsView.setAdapter(new PostsRecyclerAdapter(posts));
+                }
+            }
+
+            @Override
+            public void onFailure(@NonNull Call<List<Post>> call, @NonNull Throwable t) {
+                showProgressBar(false);
+
+                final Snackbar snackbar = Snackbar.make(postsView, t.getLocalizedMessage(), MESSAGE_SHOW_TIME);
+                snackbar.setAction(R.string.ok, view -> snackbar.dismiss());
+                snackbar.show();
+            }
+        });
+    }
+
+    private void configuringLinks(List<Post> posts, Response<List<Post>> response) {
+        String server = getServerUrl(response);
+        for (Post post : posts) {
+            String postLink = post.getLink();
+            if (postLink != null) {
+                String postUrl = server + postLink;
+                post.setLink(postUrl);
+            }
+        }
+    }
+
+    private String getServerUrl(Response<List<Post>> response) {
+        okhttp3.Response rawResponse = response.raw();
+        String url = rawResponse.request().url().toString();
+        String urlPath = rawResponse.request().url().encodedPath();
+
+        return url.substring(0, url.indexOf(urlPath));
     }
 }
